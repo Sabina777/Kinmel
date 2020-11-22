adf6693bece3eba4154340f602c02901ee01fa8a
Sabina777 <Sabina.prajapati777@gmail.com>
2020-11-22 16:37:06 +0545

    HomeScreen added, Product and rating component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
-    <div>
-      <h1>welcome to the kinmel app</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
