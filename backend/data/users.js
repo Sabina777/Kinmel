a625201270b0d84c447e959688420b5b4c768e0b
Sabina777 <Sabina.prajapati777@gmail.com>
2020-11-23 17:09:21 +0545

    users data added and seeder file added

---
+++ backend/data/users.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("12345", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Sanna Shakya",
+    email: "sanna@gmail.com",
+    password: bcrypt.hashSync("12345", 10),
+  },
+  {
+    name: "John Doe",
+    email: "john@gmail.com",
+    password: bcrypt.hashSync("12345", 10),
+  },
+];
---
+++ backend/seeder.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
+import Product from "./models/productModel.js";
+import User from "./models/userModel.js";
+import Order from "./models/orderModel.js";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
